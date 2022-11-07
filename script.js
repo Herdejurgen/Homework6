629abb95e96df24ab6db661178bdbcf6fb0ee1fd
Jack Herdejurgen <Jack.D.Herdejurgen@ou.edu>
2022-11-07 14:10:16 -0600

    function

---
+++ script.js
@@ -0,0 +1,33 @@
+function RandColor() {
+    var rand = Math.floor(Math.random() * 10);
+    if (rand==0) {
+        return "red";
+    }
+    else if (rand==1) {
+        return "orange";
+    }
+    else if (rand==2) {
+        return "yellow";
+    }
+    else if (rand == 3) {
+        return "lightgreen";
+    }
+    else if (rand == 4) {
+        return "green";
+    }
+    else if (rand == 5) {
+        return "lightblue";
+    }
+    else if (rand==6) {
+        return "blue";
+    }
+    else if (rand == 7) {
+        return "purple";
+    }
+    else if (rand == 8) {
+        return "pink";
+    }
+    else if (rand == 9) {
+        return "grey";
+    }
+}
