f551c55172aea84687aec9fcc0b0d350d24103f2
Jack Herdejurgen <Jack.D.Herdejurgen@ou.edu>
2022-11-16 12:42:07 -0600

    add canvas gradient

--- script.js
+++ script.js
@@ -7,3 +7,17 @@ anime({
     direction: 'alternate',
     easing: 'easeInOutExpo'
 });
+var granimInstance = new Granim({
+    element: '#canvas-basic',
+    direction: 'left-right',
+    isPausedWhenNotInView: true,
+    states: {
+        "default-state": {
+            gradients: [
+                ['#ff9966', '#ff5e62'],
+                ['#00F260', '#0575E6'],
+                ['#e1eec3', '#f05053']
+            ]
+        }
+    }
+});
