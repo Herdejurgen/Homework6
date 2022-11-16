190e3bbd14e344f9e83f1147bfe863eebc5c074e
Jack Herdejurgen <Jack.D.Herdejurgen@ou.edu>
2022-11-16 09:48:42 -0600

    create and svg in div1

--- script.js
+++ script.js
@@ -1,88 +1,9 @@
-document.getElementById("div1").style.backgroundColor = RandColor();
-document.getElementById("div2").style.backgroundColor = RandColor();
-document.getElementById("div3").style.backgroundColor = RandColor();
-document.getElementById("div4").style.backgroundColor = RandColor();
-
-document.getElementById("div1").innerHTML = "Hello World";
-CheckColors();
-
-function RandRandomize() {    
-    document.getElementById(RandomDiv()).style.backgroundColor = RandColor();
-    CheckColors();
-}
-
-function Blue() {
-    document.getElementById("div1").style.backgroundColor = "blue";
-    document.getElementById("div2").style.backgroundColor = "blue";
-    document.getElementById("div3").style.backgroundColor = "blue";
-    document.getElementById("div4").style.backgroundColor = "blue";
-    CheckColors();
-}
-
-function CheckColors() {
-    if (document.getElementById("div1").style.backgroundColor == document.getElementById("div2").style.backgroundColor && document.getElementById("div2").style.backgroundColor == document.getElementById("div3").style.backgroundColor && document.getElementById("div3").style.backgroundColor == document.getElementById("div4").style.backgroundColor)
-    {
-        document.getElementById("div4").innerHTML = "The colors are all the same!";
-    }
-    else {
-        document.getElementById("div4").innerHTML = "4";
-    }
-}
-
-function Randomize() {
-    document.getElementById("div1").style.backgroundColor = RandColor();
-    document.getElementById("div2").style.backgroundColor = RandColor();
-    document.getElementById("div3").style.backgroundColor = RandColor();
-    document.getElementById("div4").style.backgroundColor = RandColor();
-    CheckColors();
-}
-
-function RandomDiv() {
-    var randr = Math.floor(Math.random() * 4);
-    if (randr == 0) {
-        return "div1";
-    }
-    else if (randr == 1) {
-        return "div2";
-    }
-    else if (randr == 2) {
-        return "div3";
-    }
-    else if (randr == 3) {
-        return "div4";
-    }
-}
-
-function RandColor() {
-    var rand = Math.floor(Math.random() * 10);
-    if (rand==0) {
-        return "red";
-    }
-    else if (rand==1) {
-        return "orange";
-    }
-    else if (rand==2) {
-        return "yellow";
-    }
-    else if (rand == 3) {
-        return "lightgreen";
-    }
-    else if (rand == 4) {
-        return "green";
-    }
-    else if (rand == 5) {
-        return "lightblue";
-    }
-    else if (rand==6) {
-        return "blue";
-    }
-    else if (rand == 7) {
-        return "purple";
-    }
-    else if (rand == 8) {
-        return "pink";
-    }
-    else if (rand == 9) {
-        return "grey";
-    }
-}
+anime({
+    targets: ['.svg-attributes-demo polygon', 'feTurbulence', 'feDisplacementMap'],
+    points: '64 128 8.574 96 8.574 32 64 0 119.426 32 119.426 96',
+    baseFrequency: 0,
+    scale: 1,
+    loop: true,
+    direction: 'alternate',
+    easing: 'easeInOutExpo'
+});
