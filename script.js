2a806f98929c15664f5ba8f63d11b6afa7713fc1
Jack Herdejurgen <Jack.D.Herdejurgen@ou.edu>
2022-11-16 11:50:11 -0600

    box

--- script.js
+++ script.js
@@ -9,3 +9,20 @@ anime({
 });
 
 const Choreographer = require('choreographer-js')
+
+let choreographer = new Choreographer({
+    animations: [
+        {
+            range: [-1, window.innerWidth / 2],
+            selector: '#box',
+            type: 'change',
+            style: 'transform:translateY',
+            to: 300,
+            unit: 'px'
+        }
+    ]
+})
+
+document.body.addEventListener('mousemove', (e) => {
+    choreographer.runAnimationsAt(e.clientX)
+})
